176f5e2dff40429f007b7cee3cd03a0357598c2a
kamekichi128@gmail.com <kamekichi128@gmail.com>
2026-03-08 22:36:22 +0900

    [fix] URLの解消方法を修正

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68443122,
+    wasmFileSize: 68443389,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
