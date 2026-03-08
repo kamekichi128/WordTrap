8eb4d75f2ba1b85144e1cc2309acf007f504ae20
kamekichi128@gmail.com <kamekichi128@gmail.com>
2026-03-08 23:02:11 +0900

    [update] 実行モードをCPUに、コンテキストサイズを1024に

--- Build/Build.loader.js
+++ Build/Build.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 68443389,
+    wasmFileSize: 68443458,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
